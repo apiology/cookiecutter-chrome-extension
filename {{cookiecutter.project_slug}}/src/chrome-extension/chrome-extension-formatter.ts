47e7e17afe50ce22a43dda35642db8d91d6e24aa
Vince Broz <vince@broz.cc>
2022-06-08 08:21:36 -0400

    Move escapeHTML to formatter

--- {{cookiecutter.project_slug}}/src/chrome-extension/chrome-extension-formatter.ts
+++ {{cookiecutter.project_slug}}/src/chrome-extension/chrome-extension-formatter.ts
@@ -1,10 +1,29 @@
 {% if cookiecutter.asana_api == 'yes' -%}
 import * as Asana from 'asana';
-{% endif %-}
-import { escapeHTML } from './omnibox.js';
+{% endif -%}
+
+// How on God's green earth is there no built-in function to do this?
+//
+// https://stackoverflow.com/questions/40263803/native-javascript-or-es6-way-to-encode-and-decode-html-entities
+export const escapeHTML = (str: string) => {
+  const escape = (tag: string): string => {
+    const s = ({
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      "'": '&#39;',
+      '"': '&quot;',
+    }[tag]);
+    if (s == null) {
+      throw new Error('Error in regexp logic!');
+    }
+    return s;
+  };
+  return str.replace(/[&<>'"]/g, escape);
+};
 
 export default class ChromeExtensionFormatter {
-{-% if cookiecutter.asana_api == 'yes' %}
+{%- if cookiecutter.asana_api == 'yes' %}
   formatTask = (task: Asana.resources.Tasks.Type) => {
     const project = task.memberships[0]?.project;
 
@@ -19,5 +38,5 @@ export default class ChromeExtensionFormatter {
 
     return `${escapeHTML(task.name)}${membership}`;
   };
-{% endif %-}
+{% endif -%}
 }
