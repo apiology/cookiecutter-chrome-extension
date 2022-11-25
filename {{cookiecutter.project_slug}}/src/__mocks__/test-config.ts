f58684a3e3cf9e0c1381cb03130eef91f4163cb4
Vince Broz <vince@broz.cc>
2022-11-25 14:37:33 -0500

    Add platform mocks for reduced logging

--- {{cookiecutter.project_slug}}/src/__mocks__/test-config.ts
+++ {{cookiecutter.project_slug}}/src/__mocks__/test-config.ts
@@ -1,9 +1,9 @@
 import { Config } from '../config.js';
 
 export class TestConfig extends Config {
+{%- if cookiecutter.asana_api == 'yes' %}
   fetchAsanaAccessToken = async () => { throw new Error('not implemented'); };
 
   fetchWorkspaceName = async () => { throw new Error('not implemented'); };
-
-  fetchVldApiKey = async () => '123';
+{%- endif %}
 }
